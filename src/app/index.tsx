6f05467a19d00602c1fd3d2d9b2126eb0b4e2a55
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-03 23:16:43 +0530

    pokedex api fetch and update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  let [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20",
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setPokemon(data.results);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>Pokedex</Text>
+      <ScrollView>
+        {pokemon.map((poke) => {
+          return (
+            <View key={poke.key}>
+              <Text>{poke.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }

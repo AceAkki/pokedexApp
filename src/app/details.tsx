a58e7241075e16fa99a0a948c5cb1bc5a2994cae
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-07 23:10:47 +0530

    migration to react navigation

--- src/app/details.tsx
+++ src/app/details.tsx
@@ -1,10 +1,14 @@
 import { useLocalSearchParams } from "expo-router";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 
 const Details = () => {
   const params = useLocalSearchParams();
   // console.log(params.name);
-  return <ScrollView></ScrollView>;
+  return (
+    <ScrollView>
+      <View>{/*<Text>{params.name}</Text> */}</View>
+    </ScrollView>
+  );
 };
 export default Details;
 const styles = StyleSheet.create({});

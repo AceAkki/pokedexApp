bc47fb568bcd8fdd959893ca083c9e0833eaf293
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-07 23:00:39 +0530

    layout update

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,10 +11,15 @@ export default function RootLayout() {
       />
       <Stack.Screen
         name="details"
-        options={{
-          title: "Details",
+        options={({ route }) => ({
+          title: route.params?.name?.toLocaleUpperCase() || "Details",
           headerBackButtonDisplayMode: "minimal",
-        }}
+          presentation: "formSheet",
+          sheetAllowedDetents: [0.3, 0.5, 0.7],
+          sheetGrabberVisible: true,
+          sheetCornerRadius: 16,
+          headerShown: false,
+        })}
       />
     </Stack>
   );

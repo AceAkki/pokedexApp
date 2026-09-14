5b618a70318eb937c088d83dc96d651eac2da0a1
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-14 20:57:12 +0530

    size reduced

--- src/app/indexStyles.ts
+++ src/app/indexStyles.ts
@@ -25,9 +25,9 @@ const indexStyles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     // margin: 20,
-  },  
+  },
   promptTxt: {
-    fontSize: 35,
+    fontSize: 30,
   },
 });
 

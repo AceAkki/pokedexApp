8035fa957d048a79facbbf117c1f0a01cf0eaaa5
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-16 23:38:16 +0530

    img update

--- src/app/indexStyles.ts
+++ src/app/indexStyles.ts
@@ -28,7 +28,7 @@ const indexStyles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 16,
     resizeMode: "cover",
-    width: "100%",
+    // width: "100%",
     height: 350,
   },
   promptWrap: {

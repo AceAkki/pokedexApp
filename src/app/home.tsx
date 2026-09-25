1f2a28116175df049102cbe18b24a573774309a5
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-25 12:13:45 +0530

    flatlist fix with ListFooterComponent

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -21,7 +21,6 @@ export default function Home() {
   useEffect(() => {
     setNewPokemons(pokemons);
   }, [pokemons, newPokemons, setNewPokemons]);
-  console.log(pokemons);
 
   if (newPokemons.length === 0) {
     return <Text>Details Loading</Text>;
@@ -29,81 +28,80 @@ export default function Home() {
 
   return (
     <SafeAreaView>
-      <View style={{ flex: 1 }}>
-        <FlatList
-          data={newPokemons}
-          numColumns={2}
-          style={{ flex: 1 }}
-          columnWrapperStyle={{ gap: 16 }}
-          contentContainerStyle={{ gap: 16, padding: 20 }}
-          renderItem={({ item: poke }) => {
-            let type = poke.types[0].type.name;
-            let imageSrc = poke.sprites.other["official-artwork"]
-              .front_default as string;
-
-            return (
-              <Link
-                key={poke.name}
-                href={{ pathname: "/details", params: { name: poke.name } }}
-                style={{ flex: 1 }}
-              >
-                <ImageBackground
-                  style={[
-                    {
-                      backgroundColor:
-                        typeColors[type as keyof typeof typeColors],
-                    },
-                    globalStyles.pokemonView,
-                  ]}
-                >
-                  <Text style={globalStyles.name}>{poke.name}</Text>
-
-                  <View style={globalStyles.innerRow}>
-                    <View style={globalStyles.txtContainer}>
-                      <Text
-                        style={[
-                          globalStyles.type,
-                          {
-                            color: typeColors[type as keyof typeof typeColors],
-                            borderColor:
-                              typeColors[type as keyof typeof typeColors] + 60,
-                          },
-                        ]}
-                      >
-                        {type}
-                      </Text>
-                    </View>
-                    <ImageBackground
-                      style={globalStyles.imgContainer}
-                      source={require("../../assets/images/pokeball1.png")}
-                      resizeMode="cover"
-                      imageStyle={{ opacity: 0.3, width: 100, height: 100 }}
-                    >
-                      <Image
-                        source={{
-                          uri: imageSrc,
-                        }}
-                        style={globalStyles.image}
-                      />
-                    </ImageBackground>
-                  </View>
-                </ImageBackground>
-              </Link>
-            );
-          }}
-          keyExtractor={(poke) => poke.name}
-        />
-        <View style={{ flex: 1 }}>
-          <Pressable
-            onPress={() => {
-              let { pokemons } = useFetchData({ limit: 20 } as any);
-              setNewPokemons(pokemons);
-            }}
-          >
+      <FlatList
+        data={newPokemons}
+        numColumns={2}
+        columnWrapperStyle={{ gap: 16 }}
+        contentContainerStyle={{
+          gap: 16,
+          paddingTop: 20,
+          paddingHorizontal: 20,
+          paddingBottom: 50,
+        }}
+        ListFooterComponent={
+          <Pressable onPress={() => {}}>
             <Text>Next</Text>
           </Pressable>
-        </View>
-      </View>
+        }
+        renderItem={({ item: poke }) => {
+          let type = poke.types[0].type.name;
+          let imageSrc = poke.sprites.other["official-artwork"]
+            .front_default as string;
+
+          return (
+            <Link
+              key={poke.name}
+              href={{ pathname: "/details", params: { name: poke.name } }}
+              style={{ flex: 1 }}
+            >
+              <ImageBackground
+                style={[
+                  {
+                    backgroundColor:
+                      typeColors[type as keyof typeof typeColors],
+                  },
+                  globalStyles.pokemonView,
+                ]}
+              >
+                <Text style={globalStyles.name}>{poke.name}</Text>
+
+                <View style={globalStyles.innerRow}>
+                  <View style={globalStyles.txtContainer}>
+                    <Text
+                      style={[
+                        globalStyles.type,
+                        {
+                          color: typeColors[type as keyof typeof typeColors],
+                          borderColor:
+                            typeColors[type as keyof typeof typeColors] + 60,
+                        },
+                      ]}
+                    >
+                      {type}
+                    </Text>
+                  </View>
+                  <ImageBackground
+                    style={globalStyles.imgContainer}
+                    source={require("../../assets/images/pokeball1.png")}
+                    resizeMode="cover"
+                    imageStyle={{ opacity: 0.3, width: 100, height: 100 }}
+                  >
+                    <Image
+                      source={{
+                        uri: imageSrc,
+                      }}
+                      style={globalStyles.image}
+                    />
+                  </ImageBackground>
+                </View>
+              </ImageBackground>
+            </Link>
+          );
+        }}
+        keyExtractor={(poke) => poke.name}
+      />
+
+      <View></View>
     </SafeAreaView>
   );
 }
--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -5,23 +5,24 @@ interface hookType {
   name?: string;
   id?: string;
   limit?: number;
+  offset?: number;
 }
-const useFetchData = ({ name, id, limit }: hookType) => {
+const useFetchData = ({ name, id, limit, offset }: hookType) => {
   let [pokemons, setPokemons] = useState<Pokemon[]>([]);
   let [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
   useEffect(() => {
-    fetchPokemons({ name, id });
+    fetchPokemons({ name, id, limit, offset });
     // console.log(pokemon);
   }, []);
 
-  async function fetchPokemons({ name, id, limit = 20 }: hookType) {
+  async function fetchPokemons({ name, id, limit, offset }: hookType) {
     const hasParam = name || id;
     const url = name
       ? `https://pokeapi.co/api/v2/pokemon/${name}`
       : id
         ? `https://pokeapi.co/api/v2/pokemon/${id}`
-        : `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=0`;
+        : `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`;
     try {
       const response = await fetch(url);
       if (response.ok) {

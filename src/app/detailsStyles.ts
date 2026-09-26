d431c7d2aa7f8859a3434a6b751028db9397afa9
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-09-26 23:48:25 +0530

    details styles update

--- src/app/detailsStyles.ts
+++ src/app/detailsStyles.ts
@@ -2,26 +2,49 @@ import { colors } from "@/styles/global";
 import { StyleSheet } from "react-native";
 
 const detailsStyles = StyleSheet.create({
-  headerContainer: {
+  headingContainer: {
     width: "100%",
-    paddingHorizontal: 5,
-    paddingTop: 20,
-    alignItems: "flex-start",
-    flexDirection: "column",
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   heading: {
     fontFamily: "Oswald-Bold",
-    fontSize: 30,
+    fontSize: 40,
     color: colors.txtLight,
     backgroundColor: colors.secondary,
     textTransform: "capitalize",
-    // marginVertical: 10,
-    // alignSelf: "flex-start",
   },
+  headingLeft: {
+    justifyContent: "space-between",
+  },
+  headingRight: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  type: {
+    fontSize: 15,
+    fontFamily: "Oswald-Medium",
+    textTransform: "uppercase",
+    color: colors.text,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: colors.background,
+    alignSelf: "flex-start",
+  },
+  id: {
+    fontSize: 25,
+    fontFamily: "ArchivoBlack-Regular",
+    color: colors.txtLight,
+  },
+  imageContainer: { alignItems: "center", justifyContent: "center" },
   image: {
-    width: "100%",
+    width: 300,
     height: 300,
     aspectRatio: 1,
+    alignSelf: "center",
   },
 });
 
